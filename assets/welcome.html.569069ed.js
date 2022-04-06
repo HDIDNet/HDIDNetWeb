4bfa92c19564d39ba59018b6605367381bf1507f
paige79ferguson <paige.ferguson.79@mail.ru>
2022-04-06 11:21:05 +0800

    deploy web : V0.01:init

---
+++ assets/welcome.html.569069ed.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to HDID.Net",lang:"en-US",frontmatter:{index:1,title:"Welcome to HDID.Net",author:"HDID.Net",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`HDID Net : Hierarchical decentralized identity self-sovereign network.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://hdid.net/welcome.html"}],["meta",{property:"og:title",content:"Welcome to HDID.Net"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"HDID.Net"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>HDID Net : Hierarchical decentralized identity self-sovereign network.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"HDID Net",slug:"hdid-net",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why HDIDNet?",slug:"why-hdidnet",children:[]},{level:2,title:"Tech Stack to Build HDID Net",slug:"tech-stack-to-build-hdid-net",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649215186e3,updatedTime:1649215186e3,contributors:[{name:"paige79ferguson",email:"paige.ferguson.79@mail.ru",commits:1}]},readingTime:{minutes:.45,words:136},filePathRelative:"welcome.md"};export{e as data};
